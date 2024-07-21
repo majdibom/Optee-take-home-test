efe0775dea9ed39c9a7c3a560bcbd35a4acc2e11
mabenoth <mabenoth@bouyguestelecom.fr>
2024-07-21 19:20:10 +0200

    fix unit tests

--- pharmacy.test.js
+++ pharmacy.test.js
@@ -13,19 +13,13 @@ describe('Pharmacy', () => {
     ['Magic Pill', 10, 20, 10, 20],
     ['Dafalgan', 10, 20, 9, 18],
     ['Dafalgan', 0, 20, -1, 16],
+    ['Herbal Tea', 10, 50, 9, 50],
+    ['Fervex', 10, 52, 9, 50],
   ])('should update drug "%s" with expiresIn %i and benefit %i to expiresIn %i and benefit %i', (name, expiresIn, benefit, expectedExpiresIn, expectedBenefit) => {
     const drugs = [new Drug(name, expiresIn, benefit)];
     const pharmacy = new Pharmacy(drugs);
     pharmacy.updateBenefitValue();
     expect(drugs[0].expiresIn).toBe(expectedExpiresIn);
     expect(drugs[0].benefit).toBe(expectedBenefit);
-  });
-
-  it('should not increase the benefit of any drug above 50', () => {
-    const drugs = [new Drug('Herbal Tea', 10, 50), new Drug('Fervex', 5, 50)];
-    const pharmacy = new Pharmacy(drugs);
-    pharmacy.updateBenefitValue();
-    expect(drugs[0].benefit).toBe(50);
-    expect(drugs[1].benefit).toBe(50);
   });
 });
